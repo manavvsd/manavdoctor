5157dd934e8f54f01f9f39592fd225980ecc2fe2
manavvsd <106730490+manavvsd@users.noreply.github.com>
2022-06-02 14:13:57 +0530

    Add files via upload

---
+++ doctorsite/scrpit.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+
+
+menu.onclick = () =>{
+
+    menu.classList.toggle('fa-time');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-time');
+    navbar.classList.remove('active');
+}
